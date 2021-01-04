82cb78e4830e2b4d069511419ce63454f5572724
Telmo "Trooper <telmo.trooper@gmail.com>
2021-01-04 20:48:39 -0300

    Finished 3.5.2

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,22 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
 
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+
+    settings.merge(config::File::with_name("configuration"))?;
+
+    settings.try_into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::net::TcpListener;
-
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port.");
-
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
